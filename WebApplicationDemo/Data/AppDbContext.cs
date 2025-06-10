586dfbc2dbc4039ae0d3eb115d02c954c1c83abf
lisi <lisi@qq.com>
2025-06-10 11:24:41 +0800

    init

---
+++ WebApplicationDemo/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationDemo.Model;
+
+namespace WebApplicationDemo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ WebApplicationDemo/Program.cs
@@ -0,0 +1,40 @@
+
+using Microsoft.EntityFrameworkCore;
+using WebApplicationDemo.Data;
+
+namespace WebApplicationDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            // 添加 DbContext
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+            app.MapGet("/", () => "Web API 正常运行！");
+            app.Run();
+        }
+    }
+}
